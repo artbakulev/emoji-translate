8f666af4c0afb91598bce44cafda51bab76712a1
a.bakulev <a.bakulev@ktsstudio.ru>
2020-11-01 18:21:44 +0300

    Initial Commit

---
+++ EmojiTranslate/EmojiTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiTranslateApp.swift
+//  EmojiTranslate
+//
+//  Created by Артем Бакулев on 01.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct EmojiTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
